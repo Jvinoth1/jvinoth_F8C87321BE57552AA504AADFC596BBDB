b5b4a61622cdcc40c57841c014a1b88c72a3dbb8
Jvinoth1 <144302029+Jvinoth1@users.noreply.github.com>
2023-09-22 10:11:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def fact_rec(n):
- if n==0 or n==1:
-  return 1
- else:
-  return n*fact_rec(n-1)
-number = int(input("Enter a value : "))
-res = fact_rec(number)
-print("The factorial of {} is {}.".format(number,res))
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
